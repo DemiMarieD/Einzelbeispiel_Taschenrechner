2d6a59ae8d256dc87515c42fadb03e546edf8943
DemiMarieD <demi-marie@outlook.de>
2017-03-12 14:59:07 +0100

    Layout mit Code verknüpft. Berechnung erstellt und output an die app geleitet.

--- app/src/main/java/com/example/demi/einzelbeispiel_taschenrechner/MainActivity.java
+++ app/src/main/java/com/example/demi/einzelbeispiel_taschenrechner/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.demi.einzelbeispiel_taschenrechner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView ergebnisTextView;     //Ergebniss wird dargestellt
+
+    EditText dividendText;          //Erster operant
+    EditText divisorText;           //Zweiter operant
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View References
+        ergebnisTextView = (TextView) findViewById(R.id.ErgebnissText);
+        dividendText = (EditText) findViewById(R.id.Dividend);
+        divisorText = (EditText) findViewById(R.id.Divisor);
+
+        //Button References
+        Button calcBtn = (Button) findViewById(R.id.berechenBtn);    //entspricht in IOs IBOutlet
+        //click listener (=IBAction)
+       calcBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //Werte holen
+               Float dividend = Float.parseFloat(dividendText.getText().toString());
+               Float divisor = Float.parseFloat(divisorText.getText().toString());
+
+               //Ergebnis berechnen
+               Float ergebnis = dividend / divisor;
+
+               //an app weitergeben
+               ergebnisTextView.setText(Float.toString(ergebnis));
+
+
+           }
+       });
     }
 }
